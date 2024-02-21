2e033f6070caf3f25e302e78fc9bf694d40acff4
reisuj <reisuj@gmail.com>
2024-02-21 18:08:13 -0500

    Added basic horizontal and vertical movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _playerSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        float _verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * _horizontalInput * _playerSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * _verticalInput * _playerSpeed * Time.deltaTime);
     }
 }
