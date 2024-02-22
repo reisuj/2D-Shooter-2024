0f4f58446eaee6d3734c050fa9b583ec0ceff61a
reisuj <reisuj@gmail.com>
2024-02-21 19:08:53 -0500

    Added Vertical boundaries and Horizontal wrapping to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,10 +16,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();     
+    }
+
+    void PlayerMovement()
+    {
         float _horizontalInput = Input.GetAxis("Horizontal");
         float _verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * _horizontalInput * _playerSpeed * Time.deltaTime);
         transform.Translate(Vector3.up * _verticalInput * _playerSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.0f, 0), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
+
