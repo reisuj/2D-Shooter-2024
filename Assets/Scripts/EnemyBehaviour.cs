3100a8fa0a89cf0cc8dee8d9dd8178b4e36361af
reisuj <reisuj@gmail.com>
2024-02-22 11:33:19 -0500

    Added enemy prefab that moves down and respawns at the top after going off screen at the bottom

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -5.5f)
+        {
+            float randomX = Random.Range(-9.5f, 9.5f);
+            transform.position = new Vector3(randomX, 7.5f, 0);
+        }
+    }
+}
