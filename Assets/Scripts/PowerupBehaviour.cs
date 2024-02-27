cafaee9cdc837b1861b0ede02273e26de7c3d8f0
reisuj <reisuj@gmail.com>
2024-02-27 09:53:57 -0500

    Used switch statement to check which powerup was collected

--- Assets/Scripts/PowerupBehaviour.cs
+++ Assets/Scripts/PowerupBehaviour.cs
@@ -7,7 +7,7 @@ public class PowerupBehaviour : MonoBehaviour
     [SerializeField]
     private float _speed = 3.0f;
     [SerializeField]
-    private int PowerUpID; // 0 = TripleShot, 1 = Speed, 2 = Shield
+    private int _powerUpID; // 0 = TripleShot, 1 = Speed, 2 = Shield
     // Start is called before the first frame update
     void Start()
     {
@@ -30,20 +30,21 @@ public class PowerupBehaviour : MonoBehaviour
         if (collision.tag == "Player")
         {
             Player player = collision.transform.GetComponent<Player>();
-            if (player != null)
-            {  
-                if (PowerUpID == 0)
-                {
+
+            switch(_powerUpID)
+            {
+                case 0:
                     player.TripleShotActive();
-                }
-                else if (PowerUpID == 1)
-                {
+                    break;
+                case 1:
                     Debug.Log("Speed Powerup Collected");
-                }
-                else if (PowerUpID == 2)
-                {
+                    break;
+                case 2:
                     Debug.Log("Shield Powerup Collected");
-                }                
+                    break;
+                default:
+                    Debug.Log("Default Value");
+                    break;
             }
             Destroy(this.gameObject);
         }
