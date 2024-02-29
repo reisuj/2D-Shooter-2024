e0c8b3875953b7e9bbfdb7ba54eee5082988e9b2
reisuj <reisuj@gmail.com>
2024-02-28 22:14:49 -0500

    End of day commit

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -15,7 +15,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
         if (transform.position.y < -5.5f)
